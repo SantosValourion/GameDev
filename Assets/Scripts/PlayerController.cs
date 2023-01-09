ccb25f8d12aa6ae12a5afc966ef379a64c6c768d
Santos <48604840+SantosValourion@users.noreply.github.com>
2023-01-09 15:20:32 -0800

    Basic Distance Calculation
    For when the Player interreacts with objects is the game world, make sure that they are within a set distance.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,11 +8,13 @@ public class PlayerController : MonoBehaviour
 {
     public float moveSpeed = 1f;
     public float collisionOffset = 0.05f;
+    public float reach = 250f;
     public ContactFilter2D movementFilter;
     Vector2 movementInput;
     Rigidbody2D rb;
     List<RaycastHit2D> castCollisions = new List<RaycastHit2D>();
-    public GameObject nearistObject;
+    private GameObject nearistObject = null;
+    private Collider2D[] objects;
 
     // Start is called before the first frame update
     void Start()
@@ -34,8 +37,13 @@ public class PlayerController : MonoBehaviour
         }
     }
 
-    private GameObject getNearist(){
-        
+    private GameObject getClosest(Vector2 origin, float range){
+        objects = Physics2D.OverlapCircleAll(origin, range);
+        foreach(Collider2D obj in objects){
+           if(obj.tag != "Untagged" && obj.tag != "Player"){
+                return obj.gameObject;
+           }
+        }
         return null;
     }
 
@@ -61,14 +69,20 @@ public class PlayerController : MonoBehaviour
     }
 
     void OnInteract(){
-        nearistObject = GameObject.FindWithTag("Door");
-        switch(nearistObject.tag){
-            case "Door":
-                GameEvents.current.DoorTrigger();
-                break;
-            case "NPC":
-                GameEvents.current.DialogTrigger();
-                break;
+        nearistObject = getClosest(rb.position, reach);
+        Array.Clear(objects, 0, objects.Length);
+        if(nearistObject != null){
+            switch(nearistObject.tag){
+                case "Door":
+                    GameEvents.current.DoorTrigger();
+                    break;
+                case "NPC":
+                    GameEvents.current.DialogTrigger();
+                    break;
+                default:
+                    break;
+            }
         }
+        nearistObject = null;
     }
 }
